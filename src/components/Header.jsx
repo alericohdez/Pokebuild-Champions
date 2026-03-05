3de4095e362747be191f436f8e2622d26e24e6f4
alericohdez <ale.rico.hdez@gmail.com>
2026-03-05 16:26:09 +0000

    Actualizada tercera entrega

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../assets/logo.png';
-import Home from '../pages/Home';
 import { Link } from 'react-router-dom';
 
-
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMenu = () => setIsMenuOpen(false);
+
   return (
     <header className="header">
       <div className="header-container">
         <div className="logo">
-          <li><Link to="/"> <img src={logo} alt="PokeBuild Champions" className="header-logo-img" /> </Link></li>
+          <Link to="/" onClick={closeMenu}>
+            <img src={logo} alt="PokeBuild Champions" className="header-logo-img" />
+          </Link>
         </div>
 
-        <nav className="nav-menu">
+        <button className={`menu-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+
+        <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
           <ul>
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/tier/ubers">Ubers</Link></li>
-            <li><Link to="/tier/ou">OU</Link></li>
-            <li><Link to="/tier/uu">UU</Link></li>
-            <li><Link to="/tier/ru">RU</Link></li>
+            <li><Link to="/" onClick={closeMenu}>Inicio</Link></li>
+            <li><Link to="/tier/ubers" onClick={closeMenu}>Ubers</Link></li>
+            <li><Link to="/tier/ou" onClick={closeMenu}>OU</Link></li>
+            <li><Link to="/tier/uu" onClick={closeMenu}>UU</Link></li>
+            <li><Link to="/tier/ru" onClick={closeMenu}>RU</Link></li>
           </ul>
         </nav>
-
       </div>
     </header>
   );
